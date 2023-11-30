f3d6b27e052dafc03e693a47f73ec90d45e435e0
matnasama <110042114+matnasama@users.noreply.github.com>
2023-11-30 18:58:15 -0300

    30/11

--- src/components/Consultas/Consultas.jsx
+++ src/components/Consultas/Consultas.jsx
@@ -29,7 +29,7 @@ export default function ControlledAccordions() {
                   </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                  <Typography sx={{ paddingRight: '20px', flexShrink: 0, fontFamily: 'Futura Std' }}>
+                  <Typography sx={{ paddingRight: '20px', flexShrink: 0, fontFamily: 'Futura Std', fontWeight:200, }}>
                     {consultas.descripcion}                     
                   </Typography>
                 </AccordionDetails>
