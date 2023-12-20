56693c5f0d6bd0dc05cdccbd9216326dd3deb5ff
matnasama <110042114+matnasama@users.noreply.github.com>
2023-12-20 15:33:41 -0300

    20/12

--- src/components/Documentos/Documentos.jsx
+++ src/components/Documentos/Documentos.jsx
@@ -10,7 +10,7 @@ export default function Documentos() {
         <Typography color={'#fff'} textAlign={'left'} sx={{ fontFamily: 'Futura Std' }}>
             REGLAMENTO ACADÉMICO 2023
         </Typography>
-        <Button href="http://www.unm.edu.ar/files/Calendario-Academico-2023-UNM-Sep-2022.pdf" target="_blank" rel="noreferrer">
+        <Button href="http://www.unm.edu.ar/files/Calendario-Academico-2023-UNM-Sep-2022.pdf" target="_blank" rel="noreferrer" download>
             <DownloadIcon></DownloadIcon>
         </Button>
       </Box>
@@ -18,7 +18,7 @@ export default function Documentos() {
         <Typography color={'#fff'} textAlign={'left'} sx={{ fontFamily: 'Futura Std' }}>
         REGLAMENTO DE ALUMNOS
         </Typography>
-        <Button href="http://www.unm.edu.ar/files/Reglamento_de_Alumnos_UNM_Nov_2022_x.pdf" target="_blank" rel="noreferrer">
+        <Button href="http://www.unm.edu.ar/files/Reglamento_de_Alumnos_UNM_Nov_2022_x.pdf" target="_blank" rel="noreferrer" download>
             <DownloadIcon></DownloadIcon>
         </Button>
       </Box>
