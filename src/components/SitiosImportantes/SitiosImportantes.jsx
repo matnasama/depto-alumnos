9eeebbd37d6d5087beacec5125487203525a2ed3
matnasama <110042114+matnasama@users.noreply.github.com>
2023-12-20 15:07:10 -0300

    20/12

--- src/components/SitiosImportantes/SitiosImportantes.jsx
+++ src/components/SitiosImportantes/SitiosImportantes.jsx
@@ -8,16 +8,16 @@ import './sitiosImportantes.css'
 export default function SitiosImportantes () {
     return (
     <div className='sitios-importantes-container'>
-      {sitios.map((sitios) => (
-    <Card key={sitios.id} sx={{ width: 200, borderRadius: 0 }}>
-      <CardActionArea className='sitios-importantes-card' href={sitios.url} target='_blank'>
+      {sitios.map((sitio) => (
+    <Card key={sitio.id} sx={{ width: 200, borderRadius: 0 }}>
+      <CardActionArea className='sitios-importantes-card' href={sitio.url} target='_blank'>
         <CardMedia
           sx={{objectFit:'contain'}}
           className='sitios-importantes-card-img'
           component="img"
           height='200'
-          image={sitios.imagen}
-          alt={sitios.nombre}
+          image={sitio.imagen}
+          alt={sitio.nombre}
 
         />
       </CardActionArea>
